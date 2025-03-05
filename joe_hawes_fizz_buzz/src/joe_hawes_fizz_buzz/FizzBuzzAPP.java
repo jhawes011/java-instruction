78582999319a5d2a724168cb78e00c3577e8fbae
Joe Hawes <jhawes011@gmail.com>
2025-03-05 16:08:52 -0500

    Completed FizzBuzz

--- joe_hawes_fizz_buzz/src/joe_hawes_fizz_buzz/FizzBuzzAPP.java
+++ joe_hawes_fizz_buzz/src/joe_hawes_fizz_buzz/FizzBuzzAPP.java
@@ -1,25 +1,41 @@
 package joe_hawes_fizz_buzz;
 
+import java.util.Scanner;
+
 public class FizzBuzzAPP {
 
     public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
 	System.out.println("Welcome to the Fizz Buzz Game");
 	
 	
 	String choice = "y";
-	while (choice.equalsIgnoreCase("y"));
+	while (choice.equalsIgnoreCase("y"))
 	{
 	    
+	    System.out.print("Enter a number: ");
+	        int n = sc.nextInt();
+	        sc.nextLine();
+	        for (int i = 1; i <= n; i++) {
+	            if (i % 3 == 0 && i % 5 == 0) {
+	                System.out.println("fizzbuzz");
+	            } else if (i % 3 == 0) {
+	                System.out.println("fizz");
+	            } else if (i % 5 == 0) {
+	                System.out.println("buzz");
+	            } else {
+	                System.out.println(i);
+	            }
+	        }
 	    
 	    
 	    
-	    
-	    
+	        
 	    System.out.print("Continue?");
 	    choice = sc.nextLine();  
 	}
 	    
-
+	System.out.println("Goodbye!");
     }
 
 }
