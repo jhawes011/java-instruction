0798564662a6403c24a4c0cc35dae639058d0a55
Joe Hawes <jhawes011@gmail.com>
2025-03-07 14:37:01 -0500

    update hangman

--- joe_hawes_hangman/src/joe_hawes_hangman/HangmanApp.java
+++ joe_hawes_hangman/src/joe_hawes_hangman/HangmanApp.java
@@ -5,6 +5,72 @@ public class HangmanApp {
     public static void main(String[] args) {
 	// TODO Auto-generated method stub
 
+	
+	
+	
+	
+	
     }
-
+    
+    public static String getHangmanImage(int incorrectGuesses) {
+        return switch (incorrectGuesses) {
+            case 6 -> """
+                _____ 
+                |   | D
+                |   O  E
+                |  /|\  A
+                |  / \   D
+                |______   
+                """;
+            case 5 -> """
+                _____ 
+                |   | 
+                |   O 
+                |  /|\ 
+                |  / 
+                |______
+                """;
+            case 4 -> """
+                _____ 
+                |   | 
+                |   O 
+                |  /|\ 
+                |  
+                |______
+                """;
+            case 3 -> """
+                _____ 
+                |   | 
+                |   O 
+                |  /| 
+                |  
+                |______
+                """;
+            case 2 -> """
+                _____ 
+                |   | 
+                |   O 
+                |   | 
+                |  
+                |______
+                """;
+            case 1 -> """
+                _____ 
+                |   | 
+                |   O 
+                |  
+                |  
+                |______
+                """;
+            case 0 -> """
+                _____ 
+                |   | 
+                |  
+                |  
+                |  
+                |______
+                """;
+            default -> "";
+        };
+    }
 }
--- joe_hawes_hangman/src/joe_hawes_hangman/HangmanWords.java
+++ joe_hawes_hangman/src/joe_hawes_hangman/HangmanWords.java
@@ -1,5 +1,40 @@
 package joe_hawes_hangman;
 
-public class HangmanWords {
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
+import util.MyConsole;
+
+public class HangmanWords {
+    
+    private final List<String> words;
+    
+    public HangmanWords()
+    {
+	words = Arrays.asList("java", "code", "debug", "hangman", "program", "here", "face", "finger", "sun", "computer", "cat", "dog", "sun", "moon", "star", "fish", "book", "tree", "ball", "car",
+		"pen", "hat", "cake", "chair", "door", "lamp", "bird", "rain", "shoe", "jump",
+		"house", "smile", "apple", "grape", "cloud", "grass", "river", "table", "water", "hand",
+		"blue", "happy", "funny", "window", "school", "mouse", "phone", "orange", "dance", "story",
+		"bread", "candle", "friend", "pencil", "turtle", "butter", "guitar", "shadow", "puzzle", "rocket");
+    }
+    
+    public String getRandomWord()
+    {
+        Random rand = new Random();
+        return words.get(rand.nextInt(words.size()));
+    }
+    public String getHiddenWord(String word) 
+    {
+        return "_ ".repeat(word.length()).trim();
+    }
+    private static void displayHiddenWord(String hiddenWord) {
+	String hiddenWordDisplay = "";
+	char[] chars = hiddenWord.toCharArray();
+	for (int i = 0; i < chars.length; i++) 
+	{
+	    hiddenWordDisplay += chars[i] + " ";
+	}
+	MyConsole.printL("hidden word display = "+hiddenWordDisplay);
+    }
 }
