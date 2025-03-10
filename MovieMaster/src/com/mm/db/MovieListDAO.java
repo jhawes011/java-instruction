9f81e383637e310a650cea0afaee61873118971e
Joe Hawes <jhawes011@gmail.com>
2025-03-10 10:01:12 -0400

    init commit MovieMaster

---
+++ MovieMaster/src/com/mm/db/MovieListDAO.java
@@ -0,0 +1,45 @@
+package com.mm.db;
+
+import java.util.List;
+
+import com.mm.model.Movie;
+
+public class MovieListDAO implements DAO<Movie>
+	{
+
+	@Override
+	public List<Movie> getAll()
+		{
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+	@Override
+	public Movie getById(int id)
+		{
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+	@Override
+	public Movie add(Movie t)
+		{
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+	@Override
+	public void update(Movie t)
+		{
+			// TODO Auto-generated method stub
+			
+		}
+
+	@Override
+	public void delete(int id)
+		{
+			// TODO Auto-generated method stub
+			
+		}
+
+	}
